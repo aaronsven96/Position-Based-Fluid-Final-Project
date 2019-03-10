ca63ffde2868bf43ca4f4e89a57178350f0bb817
Aaron <aaronsven96@gmail.com>
2019-03-09 20:35:09 -0500

    added 3d interaction

--- dartmouth-phys-comp-starter-master/proj/a2_particle_physics/ParticleFluidDriver.h
+++ dartmouth-phys-comp-starter-master/proj/a2_particle_physics/ParticleFluidDriver.h
@@ -26,11 +26,24 @@ template<int d> class ParticleFluidDriver : public Driver, public OpenGLViewer
 	Bowl<d>* bowl=nullptr;
 	Sphere<d>* sphere= nullptr;
 
+	//interaction Booleans
+	bool collision_cirle = false;
+	bool water_flow = false;
+	bool set_flow = false;
+
+	//FlowCircles
+	VectorD center_new;
+	real radius_new;
+	int click = 1;
+
+	//Stop double clicking
+	bool up_click = true;
+
 public:
 	virtual void Initialize()
 	{
 		////driver initialization, initialize simulation data
-		real dx=.35;int nx=7;int ny=7;int nz=7;
+		real dx=.35;int nx=10;int ny=10;int nz=10;
 		for(int i=0;i<nx;i++){
 			for(int j=0;j<ny;j++){
 				for(int k=0;k<nz;k++){
@@ -98,30 +111,130 @@ public:
 		OpenGLViewer::Run();
 	}
 
-	////User interaction
- 	// virtual bool Mouse_Click(int left,int right,int mid,int x,int y,int w,int h)
- 	// {
- 	// 	if(left!=1){return false;}
-	// 	Vector3f win_pos = opengl_window->Project(Vector3f::Zero());
-	// 	Vector3f pos = opengl_window->Unproject(Vector3f((float)x, (float)y, win_pos[2]));
-	// 	VectorD p_pos; for (int i = 0; i < d; i++)p_pos[i] = (real)pos[i];
-	// 	sphere->center = p_pos;
-	// 	return true;
-	// 	/*Vector3f win_pos=opengl_window->Project(Vector3f::Zero());
- 	// 	Vector3f pos=opengl_window->Unproject(Vector3f((float)x,(float)y,win_pos[2]));
- 	// 	VectorD p_pos;for(int i=0;i<d;i++)p_pos[i]=(real)pos[i];
- 	// 	real r=.1*static_cast<float>(rand()%1000)/1000.+.15;
- 	// 	Add_Particle(p_pos);
- 	// 	Add_Solid_Circle(fluid.particles.Size()-1);
- 	// 	return true;*/
- 	// }
-	/*virtual bool Mouse_Drag(int x, int y, int w, int h) { 
-		Vector3f win_pos=opengl_window->Project(Vector3f::Zero());
- 		Vector3f pos=opengl_window->Unproject(Vector3f((float)x,(float)y,win_pos[2]));
- 		VectorD p_pos;for(int i=0;i<d;i++)p_pos[i]=(real)pos[i];
-		sphere->center = p_pos;
-		return ztrue; 
-	}*/
+	virtual void Initialize_Common_Callback_Keys()
+	{
+		OpenGLViewer::Initialize_Common_Callback_Keys();
+		Bind_Callback_Key('a', &Keyboard_Event_A_Func, "press A");
+
+		Bind_Callback_Key('w', &Keyboard_Event_W_Func, "press W");
+
+		Bind_Callback_Key('s', &Keyboard_Event_S_Func, "press S");
+
+		Bind_Callback_Key('d', &Keyboard_Event_D_Func, "press D");
+
+		Bind_Callback_Key('q', &Keyboard_Event_Q_Func, "press Q");
+
+		Bind_Callback_Key('e', &Keyboard_Event_E_Func, "press E");
+	}
+
+	virtual void Keyboard_Event_Q()
+	{
+		std::cout << "Q pressed" << std::endl;
+		if (fluid.XSPH_c < .2)fluid.XSPH_c += .01;
+		std::cout << fluid.XSPH_c << std::endl;
+
+	}
+
+	virtual void Keyboard_Event_E()
+	{
+		std::cout << "E pressed" << std::endl;
+		if (fluid.XSPH_c > 0)fluid.XSPH_c -= .01;
+		std::cout << fluid.XSPH_c << std::endl;
+
+	}
+
+	virtual void Keyboard_Event_A()
+	{
+		std::cout << "A pressed" << std::endl;
+		collision_cirle = !collision_cirle;
+	}
+
+	virtual void Keyboard_Event_W()
+	{
+		std::cout << "W pressed" << std::endl;
+		water_flow = !water_flow;
+	}
+	virtual void Keyboard_Event_S()
+	{
+		std::cout << "S pressed" << std::endl;
+		set_flow = !set_flow;
+		fluid.is_flow = !fluid.is_flow;
+	}
+	virtual void Keyboard_Event_D()
+	{
+		std::cout << "D pressed" << std::endl;
+		if (fluid.flow_objects.size() > 0) {
+			fluid.flow_objects.pop_back();
+			fluid.flow_direction.pop_back();
+		}
+	}
+
+	Define_Function_Object(ParticleFluidDriver, Keyboard_Event_S);
+	Define_Function_Object(ParticleFluidDriver, Keyboard_Event_A);
+	Define_Function_Object(ParticleFluidDriver, Keyboard_Event_W);
+	Define_Function_Object(ParticleFluidDriver, Keyboard_Event_D);
+	Define_Function_Object(ParticleFluidDriver, Keyboard_Event_Q);
+	Define_Function_Object(ParticleFluidDriver, Keyboard_Event_E);
+
+
+
+	virtual bool Mouse_Click(int left, int right, int mid, int x, int y, int w, int h)
+	{
+		up_click = !up_click;
+		if (left != 1 || !set_flow || up_click) { return false; }
+
+		Vector3f win_pos = opengl_window->Project(Vector3f::Zero());
+		Vector3f pos = opengl_window->Unproject(Vector3f((float)x, (float)y, win_pos[2]));
+		VectorD p_pos; for (int i = 0; i < d; i++)p_pos[i] = (real)pos[i];
+
+		switch (click) {
+		case 1: {
+			std::cout << "Pick Center \n";
+			center_new = p_pos;
+			click = 2;
+		}break;
+		case 2: {
+			std::cout << "Set Radius \n";
+			radius_new = (center_new - p_pos).norm();
+			click = 3;
+		}break;
+		case 3: {
+			std::cout << "Pick Direction \n";
+			fluid.flow_direction.push_back((p_pos - center_new) / 5);
+			Sphere<d>* sphere_new = new Sphere<d>(center_new, radius_new);
+			fluid.flow_objects.push_back(sphere_new);
+			click = 1;
+		}break;
+		}
+	}
+	virtual bool Mouse_Drag(int x, int y, int w, int h) {
+		if (!water_flow && !collision_cirle) {
+			return false;
+		}
+
+		Vector3f win_pos = opengl_window->Project(Vector3f::Zero());
+		Vector3f pos = opengl_window->Unproject(Vector3f((float)x, (float)y, win_pos[2]));
+		VectorD p_pos; for (int i = 0; i < d; i++)p_pos[i] = (real)pos[i];
+
+		if (collision_cirle) {
+			sphere->center = p_pos;
+			return true;
+		}
+		if (water_flow) {
+			real r = (rand() % 5) - 2.5;
+			Add_Particle(p_pos);
+			Add_Solid_Circle(fluid.particles.Size() - 1);
+			VectorD vel;
+			for (int i = 0; i < d; i++) {
+				vel[i] = p_pos[i];
+			}
+			vel[1] += 5;
+			vel[0] += r;
+
+			fluid.particles.V(fluid.particles.Size() - 1) = vel;
+			return true;
+		}
+	}
 
 protected:
 	void Add_Particle(VectorD pos,real m=1.)
